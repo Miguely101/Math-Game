1f09aa9db82e15522c9fa5b627ea524e084d7841
Miguely101 <93840262+Miguely101@users.noreply.github.com>
2022-04-18 02:43:19 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
     cors: {
-        origin: '["http://localhost:4200"]',
-    },
+        origin: '*'
+    }
 });
 
 
